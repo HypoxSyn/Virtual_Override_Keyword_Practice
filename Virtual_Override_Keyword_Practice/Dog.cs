953965c626931290ead45301daaf5a287dc0d778
Chaz Hall <chaz.h.hall@gmail.com>
2022-07-06 11:26:14 -0600

    Add project files.

---
+++ Virtual_Override_Keyword_Practice/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Virtual_Override_Keyword_Practice
+{
+    class Dog: Animal
+    {
+        public bool IsHappy { get; set; }
+        public Dog(string name, int age) : base(name,age)
+        {
+            IsHappy = true;
+        }
+
+        public virtual void Play()
+        {
+            Console.WriteLine($"{Name} is playing.");
+        }
+
+
+
+    }
+}
